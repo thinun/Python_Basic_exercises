9a523dfb8fa13613aae4558face80cded40bf31e
thinun <Thinuntharushika@gmail.com>
2024-03-18 09:24:12 +0530

    initial file

--- main.py
+++ main.py
@@ -5,8 +5,8 @@
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakp
+    # Use a breakpoint .
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the
 
 
 # Press the green button in the gutter to run the script.
