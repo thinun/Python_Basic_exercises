2e9dc0e07bf65a3a9c32a0d20c7c3d69c87199b1
thinun <Thinuntharushika@gmail.com>
2024-03-21 16:19:34 +0530

    folder  d

--- basic_questions/qustion_1.py
+++ basic_questions/qustion_1.py
@@ -1,1 +1,6 @@
-print('hello world')
+previous_number = 0
+
+for number in range(1, 11):
+    print(
+        f"current number is:{number} previous number is:{previous_number} Sum of the numbers is: {previous_number + number}")
+    previous_number = previous_number + 1
