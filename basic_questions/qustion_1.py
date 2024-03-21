8cee59c485e318eb8adeb170c476bc3cbc96c342
thinun <Thinuntharushika@gmail.com>
2024-03-21 16:19:55 +0530

    Question 1

--- basic_questions/qustion_1.py
+++ basic_questions/qustion_1.py
@@ -4,3 +4,4 @@ for number in range(1, 11):
     print(
         f"current number is:{number} previous number is:{previous_number} Sum of the numbers is: {previous_number + number}")
     previous_number = previous_number + 1
+
