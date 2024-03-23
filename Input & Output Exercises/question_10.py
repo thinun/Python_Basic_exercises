5faa286d72e45a4376900b3e2b59e540ef9ce7e8
thinun <75312054+thinun@users.noreply.github.com>
2024-03-23 20:33:21 +0530

    question_10.py

---
+++ Input & Output Exercises/question_10.py
@@ -0,0 +1,3 @@
+with open('txt_1.txt', 'r') as file:
+    lines = file.readlines()
+    print(lines[3])
--- basic_questions/test.py
+++
@@ -1,8 +0,0 @@
-try:
-    user_1, user_2, user_3 = input('Enter two numbers separated by space: ').split(' ')
-
-    print(user_1)
-    print(user_2)
-    print(user_3)
-except ValueError:
-    print('Please enter 3 values')
