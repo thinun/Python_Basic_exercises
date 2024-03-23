85158497114fb33adf056c46e4ad928f48013357
thinun <75312054+thinun@users.noreply.github.com>
2024-03-23 16:49:16 +0530

    question_5.py

--- Input & Output Exercises/question_3.py
+++ Input & Output Exercises/question_3.py
@@ -1,7 +1,11 @@
 num = 8
 print(oct(num)[2:])
 
-
-# another way
+# Another way
 num = 8
 print('%o' % num)
+
+# Another way
+
+num = 8
+print(f'{num:02o}')
--- Input & Output Exercises/question_5.py
+++ Input & Output Exercises/question_5.py
@@ -1,14 +1,16 @@
 num_list = []
 
+
 def floating_num():
     i = 0
     while i < 3:
         try:
             user_input = float(input("Enter a floating number: "))
             num_list.append(user_input)
-            i += 1  # Increment the loop counter only when a valid float is entered
+            i += 1
         except ValueError:
             print("Please enter only numbers!!!")
+
 
 floating_num()
 print(num_list)
