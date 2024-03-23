f395411bb5966c5dd9fdccf1ee6b4a245fd9cd54
thinun <75312054+thinun@users.noreply.github.com>
2024-03-23 17:08:26 +0530

    question_5.py

--- basic_questions/test.py
+++ basic_questions/test.py
@@ -1,5 +1,6 @@
 num_list = []
 
+
 def floating_num():
     i = 0
     while i < 3:
@@ -9,6 +10,7 @@ def floating_num():
             i += 1  # Increment the loop counter only when a valid float is entered
         except ValueError:
             print("Please enter only numbers!!!")
+
 
 floating_num()
 print(num_list)
