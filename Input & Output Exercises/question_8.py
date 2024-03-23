491919f179ad8edd0e309ad74fc457ad9c4841de
thinun <75312054+thinun@users.noreply.github.com>
2024-03-23 19:34:49 +0530

    question_8.py

---
+++ Input & Output Exercises/question_8.py
@@ -0,0 +1,9 @@
+totalMoney = 1000
+quantity = 3
+price = 450
+
+print(f'I have {totalMoney} dollars so I can buy {quantity} football for {price:.2f} dollars.')
+
+# Another way
+
+print('I have {0} dollars so I can buy {1} football for {2} dollars.'.format(totalMoney, quantity, price))
